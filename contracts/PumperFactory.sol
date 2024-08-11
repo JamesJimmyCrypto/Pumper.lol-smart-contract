bae999dd1d20f63a37f4ca22447d2639d5da0f03
new dapp sync <oluwafemialofe@yahoo.com>
2024-08-11 21:52:14 +0400

    Only Pumper token can call emit

--- contracts/PumperFactory.sol
+++ contracts/PumperFactory.sol
@@ -11,6 +11,14 @@ contract PumperFactory {
     event Sell(address indexed seller, uint256 amount, uint256 price);
     event LiquidtyDeployed(address indexed pool, uint256 x, uint256 y);
 
+    modifier onlyPumperToken() {
+        require(
+            deployedPumpTokens[msg.sender].length > 0,
+            "Only PumperToken can call this function"
+        );
+        _;
+    }
+
     function createPumpToken(string memory name, string memory symbol) public {
         address newPumpToken = address(
             new PumperToken(name, symbol, address(this))
@@ -22,15 +30,27 @@ contract PumperFactory {
         return deployedPumpTokens[msg.sender];
     }
 
-    function emitBuy(address buyer, uint256 amount, uint256 price) public {
+    function emitBuy(
+        address buyer,
+        uint256 amount,
+        uint256 price
+    ) public onlyPumperToken {
         emit Buy(buyer, amount, price);
     }
 
-    function emitSell(address seller, uint256 amount, uint256 price) public {
+    function emitSell(
+        address seller,
+        uint256 amount,
+        uint256 price
+    ) public onlyPumperToken {
         emit Sell(seller, amount, price);
     }
 
-    function emitLiquidtyDeployed(address pool, uint256 x, uint256 y) public {
+    function emitLiquidtyDeployed(
+        address pool,
+        uint256 x,
+        uint256 y
+    ) public onlyPumperToken {
         emit LiquidtyDeployed(pool, x, y);
     }
 }
